065137f49ec7f3f0fe26a0e22dea9485fa23865c
RomainDW <romain.ollier34@gmail.com>
2019-06-18 21:47:56 +0200

    Translation of the task form labels

--- src/AppBundle/Form/TaskType.php
+++ src/AppBundle/Form/TaskType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
@@ -11,11 +12,14 @@ class TaskType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
+            ->add('title', TextType::class, array(
+                'label' => 'Titre'
+            ))
             ->add('content', TextareaType::class, array(
-               'attr' => array(
+                'label' => 'Contenu',
+                'attr' => array(
                    'rows' => 5
-               )
+                )
             ))
             //->add('author') ===> must be the user authenticated
         ;
