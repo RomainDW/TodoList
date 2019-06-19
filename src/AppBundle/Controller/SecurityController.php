2480ff8369715152446b8f7c0f6085eb078c42ae
RomainDW <romain.ollier34@gmail.com>
2019-06-19 22:33:57 +0200

    Add login check & logout in the security controller

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -27,4 +27,20 @@ class SecurityController extends Controller
             'error'         => $error,
         ));
     }
+
+    /**
+     * @Route("/login_check", name="login_check")
+     */
+    public function loginCheck()
+    {
+        // This code is never executed.
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logoutCheck()
+    {
+        // This code is never executed.
+    }
 }
