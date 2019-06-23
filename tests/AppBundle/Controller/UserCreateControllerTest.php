f196950a35242903bf114954fbd434bf91ab3f86
RomainDW <romain.ollier34@gmail.com>
2019-06-23 18:42:44 +0200

    [Unit test] : test UserCreateController

---
+++ tests/AppBundle/Controller/UserCreateControllerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Tests\AppBundle\Controller;
+
+use Tests\AppBundle\UnitTestCase;
+
+class UserCreateControllerTest extends UnitTestCase
+{
+    public function testSecure()
+    {
+        //User not logged
+        $this->client->request('GET', '/users/create');
+        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+        $this->assertTrue($this->client->getResponse()->isRedirect('/login'));
+
+        //User logged with role "ROLE_USER"
+        $this->login(['ROLE_USER']);
+        $this->client->request('GET', '/users/create');
+        $this->assertEquals(403, $this->client->getResponse()->getStatusCode());
+
+        //User logged with role "ROLE_ADMIN"
+        $this->login(['ROLE_ADMIN']);
+        $this->client->request('GET', '/users/create');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testFormRendering()
+    {
+        $this->logIn();
+        $crawler = $this->client->request('GET', '/users/create');
+
+        $this->assertEquals(1, $crawler->filter('form')->count());
+    }
+}
