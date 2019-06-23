ffd4972d71dd74715e8bb5ab443e755c4e4cfb5f
RomainDW <romain.ollier34@gmail.com>
2019-06-23 14:00:35 +0200

    [Login] : Redirect to the homepage if the user is already logged

---
+++ tests/AppBundle/Controller/SecurityControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace Tests\AppBundle\Controller;
+
+use Tests\AppBundle\UnitTestCase;
+
+class SecurityControllerTest extends UnitTestCase
+{
+    public function testFormRendering()
+    {
+        $crawler = $this->client->request('GET', '/login');
+
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $this->assertEquals(1, $crawler->filter('form')->count());
+    }
+
+    public function testRedirectIfLogged()
+    {
+        $this->logIn();
+
+        $this->client->request('GET', '/login');
+
+        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+    }
+}
