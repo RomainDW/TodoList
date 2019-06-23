0b8b39de68e0a1a1beef438d97bd53312ceb3d86
RomainDW <romain.ollier34@gmail.com>
2019-06-23 17:59:32 +0200

    [Unit test] : test TaskDeleteController

---
+++ tests/AppBundle/Controller/TaskDeleteControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace Tests\AppBundle\Controller;
+
+use Tests\AppBundle\UnitTestCase;
+
+class TaskDeleteControllerTest extends UnitTestCase
+{
+    public function testSecure()
+    {
+        //Method not allowed
+        $this->client->request('GET', '/tasks/1/delete');
+        $this->assertEquals(405, $this->client->getResponse()->getStatusCode());
+
+        //User not logged
+        $this->client->request('POST', '/tasks/1/delete');
+        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+        $this->assertTrue($this->client->getResponse()->isRedirect('/login'));
+
+        //User logged with role "IS_AUTHENTICATED_ANONYMOUSLY"
+        $this->login(['IS_AUTHENTICATED_ANONYMOUSLY']);
+        $this->client->request('POST', '/tasks/1/delete');
+        $this->assertEquals(403, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testTaskNotFound()
+    {
+        $this->client->request('POST', '/tasks/999/delete');
+        $this->assertEquals(404, $this->client->getResponse()->getStatusCode());
+    }
+}
