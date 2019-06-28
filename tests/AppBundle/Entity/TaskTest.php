7b734534dc04171f450ca79fd21d4d360be2712a
RomainDW <romain.ollier34@gmail.com>
2019-06-28 12:12:43 +0200

    [Functional tests] : test Entities with PHPUnit

--- tests/AppBundle/Entity/TaskTest.php
+++ tests/AppBundle/Entity/TaskTest.php
@@ -6,8 +6,9 @@ namespace Tests\AppBundle\Entity;
 use AppBundle\Entity\Task;
 use AppBundle\Entity\User;
 use PHPUnit\Framework\TestCase;
+use Tests\AppBundle\MyTestCase;
 
-class TaskTest extends TestCase
+class TaskTest extends MyTestCase
 {
     public function testImplementation()
     {
@@ -21,8 +22,54 @@ class TaskTest extends TestCase
 
         $task = new Task($title, $content, $user);
 
+        $this->entityManager->persist($user);
+        $this->entityManager->persist($task);
+        $this->entityManager->flush();
+
         $this->assertSame($title, $task->getTitle());
         $this->assertSame($content, $task->getContent());
         $this->assertSame($user, $task->getUser());
+        $this->assertNotEmpty($task->getId());
+    }
+
+    public function testToggle()
+    {
+        $title = 'test';
+        $content = 'test';
+        $user = new User(
+            'test',
+            'test',
+            'admin@email.com.com'
+        );
+
+        $task = new Task($title, $content, $user);
+
+        $task->toggle(false);
+        $this->assertFalse($task->isDone());
+
+        $task->toggle(true);
+        $this->assertTrue($task->isDone());
+    }
+
+    public function testUpdate()
+    {
+        $title = 'test';
+        $content = 'test';
+        $user = new User(
+            'test',
+            'test',
+            'admin@email.com.com'
+        );
+        $task = new Task($title, $content, $user);
+
+        $titleUpdate = 'testUpdate';
+        $contentUpdate = 'testUpdate';
+        $task->update(
+            $titleUpdate,
+            $contentUpdate
+        );
+
+        $this->assertEquals($titleUpdate, $task->getTitle());
+        $this->assertEquals($contentUpdate, $task->getContent());
     }
 }
--- tests/AppBundle/Entity/UserTest.php
+++ tests/AppBundle/Entity/UserTest.php
@@ -5,19 +5,57 @@ namespace Tests\AppBundle\Entity;
 
 use AppBundle\Entity\User;
 use PHPUnit\Framework\TestCase;
+use Tests\AppBundle\MyTestCase;
 
-class UserTest extends TestCase
+class UserTest extends MyTestCase
 {
     public function testImplementation()
     {
         $username = 'test';
         $password = 'test';
-        $email = 'admin@email.com.com';
+        $email = 'test@email.com';
 
         $user = new User($username, $password, $email);
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
 
         $this->assertSame($username, $user->getUsername());
         $this->assertNotSame($password, $user->getPassword());// password is hashed
         $this->assertSame($email, $user->getEmail());
+        $this->assertNotEmpty($user->getId());
+        $this->assertNull($user->getSalt());
+        $this->assertEquals(['ROLE_USER'], $user->getRoles());
+    }
+
+    public function testUpdate()
+    {
+        $username = 'test';
+        $password = 'test';
+        $email = 'test@email.com';
+        $user = new User($username, $password, $email);
+
+        $usernameUpdate = 'testUpdate';
+        $passwordUpdate = 'testUpdate';
+        $emailUpdate = 'testUpdate@email.com';
+        $user->update(
+            $usernameUpdate,
+            $passwordUpdate,
+            $emailUpdate
+        );
+
+        $this->assertSame($usernameUpdate, $user->getUsername());
+        $this->assertNotSame($passwordUpdate, $user->getPassword());// password is hashed
+        $this->assertSame($emailUpdate, $user->getEmail());
+    }
+
+    public function testEraseCredentials()
+    {
+        $username = 'test';
+        $password = 'test';
+        $email = 'test@email.com';
+        $user = new User($username, $password, $email);
+
+        $user->eraseCredentials();
+        $this->assertNull($user->getPassword());
     }
 }
