6f8825cf7f5af83243124a4496bd00b57b62d39d
RomainDW <romain.ollier34@gmail.com>
2019-06-28 14:04:40 +0200

    [Functional tests] : test repositories with PHPUnit

--- tests/AppBundle/Repository/TaskRepositoryTest.php
+++ tests/AppBundle/Repository/TaskRepositoryTest.php
@@ -4,8 +4,11 @@
 namespace Tests\AppBundle\Repository;
 
 use AppBundle\Entity\Task;
+use AppBundle\Repository\TaskRepository;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepositoryInterface;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\NonUniqueResultException;
+use Symfony\Bridge\Doctrine\RegistryInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class TaskRepositoryTest extends KernelTestCase
@@ -14,14 +17,24 @@ class TaskRepositoryTest extends KernelTestCase
      * @var EntityManager
      */
     private $entityManager;
+    private $doctrine;
 
     protected function setUp()
     {
         $kernel = self::bootKernel();
+        $this->doctrine = $kernel->getContainer()->get('doctrine');
+        $this->entityManager = $this->doctrine->getManager();
+    }
 
-        $this->entityManager = $kernel->getContainer()
-            ->get('doctrine')
-            ->getManager();
+    public function testDependencies()
+    {
+        $this->assertInstanceOf(RegistryInterface::class, $this->doctrine);
+        $taskRepository = new TaskRepository($this->doctrine);
+
+        $this->assertInstanceOf(
+            ServiceEntityRepositoryInterface::class,
+            $taskRepository
+        );
     }
 
     public function testFindByPage()
